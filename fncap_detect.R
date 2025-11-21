23f13c011cd0d5eb5a44a2a3f0042d4070d5167c
Andrew Steinkruger <amsteinkruger@gmail.com>
2025-11-21 13:54:39 -0800

    Set-up for USGS EE.

--- fncap_detect.R
+++ fncap_detect.R
@@ -11,7 +11,11 @@ dat_boundaries =
   "data/data_boundaries.gdb" %>% 
   vect %>% 
   filter(STATENAME == "Oregon" & NAME == "Lane") %>% 
-  project("EPSG:2992") # For FERNS.
+  project("EPSG:3857") # For FERNS.
+
+dat_boundaries_4326 = dat_boundaries %>% project("EPSG:4326")
+
+ext_boundaries = dat_boundaries_4326 %>% ext
 
 # Get FERNS data.
 
@@ -20,10 +24,12 @@ dat_boundaries =
 dat_notifications = 
   "data/data_notifications.gdb" %>% 
   vect %>% 
+  filter(ActivityType == "Clearcut/Overstory Removal") %>% 
+  filter(ActivityUnit == "MBF") %>% 
+  filter(LandOwnerType == "Partnership/Corporate Forestland Ownership") %>% 
+  project("EPSG:3857") %>% # Redundant.
   crop(dat_boundaries)
 
 # Get Landsat data.
-#  Note luna in rspatial ecosystem
-#  and LandsatTS
 
-getLandsat()
+
